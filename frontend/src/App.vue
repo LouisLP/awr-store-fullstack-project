1a056d49e5c6f82601506b894780467f5cdbdbbd
LouisLP <louis.lascelles.palys@gmail.com>
2025-12-04 13:14:54 +0100

    Copy-pasting my template for frontend Vue projects

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Footer from '@/components/ui/BaseFooter.vue'
+import Navbar from '@/components/ui/BaseNavbar.vue'
+</script>
+
+<template>
+  <div class="min-h-screen flex flex-col">
+    <Navbar />
+
+    <main class="flex-1">
+      <RouterView />
+    </main>
+
+    <Footer />
+  </div>
+</template>
