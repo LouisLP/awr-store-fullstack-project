c2d734660333f70c8aaa72d555be2a39d8737213
LouisLP <louis.lascelles.palys@gmail.com>
2025-12-04 13:34:03 +0100

    API for the 2 (current) operations on products

---
+++ frontend/src/api/product.ts
@@ -0,0 +1,15 @@
+import type { CreateProductDto, Product } from '@/types/product'
+
+import apiClient from './client'
+
+export const productsApi = {
+  async getAll(): Promise<Product[]> {
+    const { data } = await apiClient.get<Product[]>('/products')
+    return data
+  },
+
+  async create(product: CreateProductDto): Promise<Product> {
+    const { data } = await apiClient.post<Product>('/products', product)
+    return data
+  },
+}
